b75197afe4e8df298dc43f48d54747ed204082bd
Marcus Östling <mpt.ostling@gmail.com>
2019-01-14 13:23:57 +0100

    tldr

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# Install packages
+sudo apt install git
+sudo apt install curl
+sudo apt install tldr # nice man pages
--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt install git
-sudo apt install curl
 sudo apt install zsh
 
 #OhMyZsh
