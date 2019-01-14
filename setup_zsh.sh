69d3f70a4efba7e5980b0cdac9d24cd399ea964c
Marcus Östling <mpt.ostling@gmail.com>
2019-01-14 08:28:15 +0100

    Add OhMyZsh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+sudo apt install curl
 sudo apt install zsh
+
+#OhMyZsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # base16 colors
 # https://github.com/chriskempson/base16-shell
