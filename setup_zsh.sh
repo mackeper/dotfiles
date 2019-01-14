54023242f2407a3d930a93d329970221ea209320
Marcus Östling <mpt.ostling@gmail.com>
2019-01-14 11:05:28 +0100

    Remove comment

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -13,15 +13,3 @@ git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shel
 
 cp .bashrc_WSL ~/.bashrc
 cp .zshrc ~/.zshrc
-
-# Install powerline font
-# https://github.com/powerline/fonts
-# Window: Download as ZIP
-# extract
-# Start powershell with admin
-# run: Set-ExecutionPolicy Bypass
-# Navigate to the folder
-# run: .\install.ps1
-# run: Set-ExecutionPolicy Default
-
-# Run :PlugInstall in nvim
