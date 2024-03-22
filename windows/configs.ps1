386a5683ce5b46db65cb441b1c1b80e1550c4f21
Marcus Östling <mpt.ostling@gmail.com>
2024-03-22 21:26:38 +0100

    start on some windows scripts

---
+++ windows/configs.ps1
@@ -0,0 +1,2 @@
+mklink ~/Documents/PowerShell/Microsoft.PowerShell_profile.ps1 ~/git/dotfiles/windows/Powershell/Microsoft.PowerShell_profile.ps1
+
---
+++ windows/packages.ps1
@@ -0,0 +1,32 @@
+# list of packages
+packages = @()
+
+## Development
+packages += 'vscode'
+packages += 'neovim'
+
+packages += 'git'
+packages += 'mingw'
+packages += 'microsoft-windows-terminal'
+
+## Programming Languages
+packages += 'python'
+packages += 'nodejs'
+packages += 'elm-platform'
+packages += 'rustup.install'
+packages += 'dotnet-sdk'
+packages += 'autohotkey'
+
+## Browsers
+packages += 'firefox'
+packages += 'googlechrome'
+
+## Communication
+packages += 'discord'
+
+## Tools
+packages += '7zip'
+packages += 'fzf'
+packages += 'ripgrep'
+packages += 'powertoys'
+packages += 'bottom'
