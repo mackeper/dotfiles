4dd2812c8044d1c3624175fbd5b986d8ac6910a9
Marcus Östling <mpt.ostling@gmail.com>
2025-06-24 15:03:06 +0200

    auto-commit: /home/marcus/git/dotfiles/nvim/lua/marcus/init.lua

--- nvim/lua/marcus/init.lua
+++ nvim/lua/marcus/init.lua
@@ -1,5 +1,7 @@
 require("marcus.set")
 require("marcus.lazy")
-require("marcus.remap")
+require("marcus.r emap")
 require("marcus.markdown2pdf")
+require("marcus.globals")
+require("marcus.autocmd")
 -- require("marcus.session-plugin")
