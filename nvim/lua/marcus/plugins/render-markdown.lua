455eb7826f565180689433c3ac0e6b043b21d477
Marcus Östling <mpt.ostling@gmail.com>
2025-10-27 22:38:25 +0100

    Update render

--- nvim/lua/marcus/plugins/render-markdown.lua
+++ nvim/lua/marcus/plugins/render-markdown.lua
@@ -1,10 +1,12 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
-
         file_types = {
             "markdown",
             "copilot-chat",
+        },
+        anti_conceal = {
+            enabled = false,
         },
     },
     dependencies = {
