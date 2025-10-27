11f3c7cc49b589acf3158432fe11eb2ce9bb6c43
Marcus Östling <mpt.ostling@gmail.com>
2025-10-27 15:30:25 +0100

    Git add doodle

---
+++ nvim/lua/marcus/plugins/doodle.lua
@@ -0,0 +1,36 @@
+return {
+    enabled = true,
+    "apdot/doodle", --  Your second brain, inside Neovim.
+    -- dir = "/home/marcus/git/doodle",
+    dependencies = {
+        "kkharji/sqlite.lua",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+        require("doodle").setup({
+            settings = {
+                git_repo = "/home/marcus/git/doodle_db",
+                sync = true,
+                auto_save = true,
+            }
+        })
+    end,
+    keys = {
+        {
+            "<space>df",
+            function() require("doodle"):toggle_finder() end,
+            desc = "Doodle Finder"
+        },
+        {
+            "<space>ds",
+            function() require("doodle"):sync() end,
+            desc = "Doodle Sync"
+        },
+        {
+            "<space>dl",
+            function() require("doodle"):toggle_links() end,
+            desc = "Doodle Links"
+        },
+    },
+}
