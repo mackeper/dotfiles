12411546072553e63d5c87aea4f328cd6ee01425
Marcus Östling <mpt.ostling@gmail.com>
2025-12-08 20:05:52 +0100

    Enable vim-tmux-navigator in insert mode

--- nvim/lua/marcus/plugins/vim-tmux-navigator.lua
+++ nvim/lua/marcus/plugins/vim-tmux-navigator.lua
@@ -10,11 +10,18 @@ return {
         "TmuxNavigatePrevious",
     },
     keys = {
+        -- Normal mode
         { "<C-LEFT>", "<CMD>TmuxNavigateLeft<cr>" },
         { "<C-DOWN>", "<CMD>TmuxNavigateDown<cr>" },
         { "<C-UP>", "<CMD>TmuxNavigateUp<cr>" },
         { "<C-RIGHT>", "<CMD>TmuxNavigateRight<cr>" },
         { "<M-\\>", "<CMD>TmuxNavigatePrevious<cr>" },
+
+        -- Insert mode
+        { "<C-LEFT>", "<CMD>TmuxNavigateLeft<cr>", mode = "i" },
+        { "<C-DOWN>", "<CMD>TmuxNavigateDown<cr>", mode = "i" },
+        { "<C-UP>", "<CMD>TmuxNavigateUp<cr>", mode = "i" },
+        { "<C-RIGHT>", "<CMD>TmuxNavigateRight<cr>", mode = "i" },
     },
     config = function()
         vim.g.tmux_navigator_no_mappings = 1
