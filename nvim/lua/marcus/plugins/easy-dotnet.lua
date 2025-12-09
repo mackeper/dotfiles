599ec03d1922bee276cfc8d0811693903eeb3e9a
Marcus Östling <marost@raysearchlabs.com>
2025-12-09 13:18:38 +0100

    remove telescope from easy-dotnet

--- nvim/lua/marcus/plugins/easy-dotnet.lua
+++ nvim/lua/marcus/plugins/easy-dotnet.lua
@@ -1,7 +1,7 @@
 return {
     "GustavEikaas/easy-dotnet.nvim",
     ft = "cs",
-    dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
+    dependencies = { "nvim-lua/plenary.nvim", },
     config = function()
         require("easy-dotnet").setup()
     end
